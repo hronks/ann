d20139ed5ff17e31fdd8adba244a24eac8c5970a
hronks <harry@ronks.org>
2020-09-01 14:34:16 +0100

    010920

--- main.cpp
+++ main.cpp
@@ -9,35 +9,47 @@ int main() {
   x.push_back({1, -2, 3});
   x.push_back({-4, 5, -6});
   x.push_back({7, -8, 9});
-  u = {-1, 2, -3};
-  v = {4, -5, 6};
-  w = {-7, 8, -9};
+
+  u = {8450, 7, 5, 856, 2, 1, 3, 8, 0, 548};
+  v = {0.232};
+
   f = {& ReLU <float>, & ReLU <float>, & ReLU <float>};
 
-  Dense_layer <float> l (3, 3, & ReLU <float>);
-  
-  l.input = u;
-  l.weight = x;
-  l.bias = w;
-  l.activation = f;
-  l.output = v;
 
-  l.d_output = v;
-  l.transpose_weight = x;
+  Dense_layer <float> l1 (10, 32, & ReLU    <float>);
+  Dense_layer <float> l2 (32, 32, & ReLU    <float>);
+  Dense_layer <float> l3 (32, 1,  & Sigmoid <float>);
 
-  l.pass_back_inbox = v;
-  l.pass_back_outbox = v;
+  l1.input = & u;
+  l1.pass_back_inbox = & l2.pass_back_outbox;
+  l1.randomize_weights(0);
 
-  l.d_weight = x;
-  l.d_bias = v;
+  l2.input = & l1.output;
+  l2.pass_back_inbox = & l3.pass_back_outbox;
+  l2.randomize_weights(0);
 
-  print(l.input);
-  print(l.weight);
-  print(l.bias);
+  l3.input = & l2.output;
+  l3.pass_back_inbox = & v;
+  l3.randomize_weights(0);
 
-  l.forward();
-  l.backwards();
-  l.learn(0.000001);
+
+  std::cout<<"\nLAYER 1\n";
+  std::cout<<"weight\n"; print(l1.weight);
+  std::cout<<"bias\n"; print(l1.bias);
+
+  std::cout<<"\nLAYER 2\n";
+  std::cout<<"weight\n"; print(l2.weight);
+  std::cout<<"bias\n"; print(l2.bias);
+
+  std::cout<<"\nLAYER 3\n";
+  std::cout<<"weight\n"; print(l3.weight);
+  std::cout<<"bias\n"; print(l3.bias);
+
+
+
+  l1.forward();
+  l1.backwards();
+  l1.learn(0.000001);
 
 
 }
